e52759fb3e25be0ef19b17fa0e88694b934b0cb5
Omar <omaressamouf@icloud.com>
2026-08-08 19:18:51 +0300

    fix auth callback default redirect

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -7,8 +7,7 @@ import { createServerSupabaseClient } from '@/lib/supabase/server'
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url)
   const code = searchParams.get('code')
-  const next = searchParams.get('next') || '/'
-
+  const next = searchParams.get('next') || '/login?confirmed=true'
   if (code) {
     const supabase = createServerSupabaseClient()
     await supabase.auth.exchangeCodeForSession(code)
